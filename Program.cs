4e8cfde5dc69611f4e1840308f66c36e82c42ff8
TungNguyenAnh <114374972+TungNguyenAnh@users.noreply.github.com>
2024-04-09 13:46:33 +0700

    su_Dung_Toan_Tu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿DateTime localDate = DateTime.Now;
-System.Console.WriteLine("Datetime Now is :" + localDate);
+﻿float witdh;
+float height;
+
+System.Console.WriteLine("Enter witdh: ");
+witdh = float.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter height: ");
+height = float.Parse(Console.ReadLine());
+
+float area = witdh * height;
+
+System.Console.WriteLine("Area is :" + area);
+
